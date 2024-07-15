cd0ee971c7857e0bac58655079cc18b7cf130b7e
VedantBhosle31 <ae22b062@smail.iitm.ac.in>
2024-07-15 15:29:11 +0530

    Added Function to download all the files in the /tmp

--- pkg/sharepoint/sharepoint_api.py
+++ pkg/sharepoint/sharepoint_api.py
@@ -1,4 +1,6 @@
 from config.config_loader import appconfig
+from office365.sharepoint.files.file import File
+import os
 
 
 def list_all_folders(ctx):
@@ -23,3 +25,20 @@ def list_all_files(ctx):
         while len(current_folder_folders) > 0:
             folders.append(current_folder_folders.pop())
     return files
+
+
+def save_file(ctx, file):
+    parent_dir = file.parent_collection.parent.serverRelativeUrl
+    file_n = file.name
+    file_obj = File.open_binary(ctx, file.serverRelativeUrl).content
+    file_dir_path = appconfig["app"]["downloadDirectory"] + parent_dir + "/" + file_n
+    os.makedirs(os.path.dirname(file_dir_path), exist_ok=True)
+    with open(file_dir_path, "wb") as f:
+        f.write(file_obj)
+    return
+
+
+def download_all_files(ctx, files):
+    for file in files:
+        save_file(ctx, file)
+    return
