baf621edd3afb445c15b0d97d9745a10cff24379
VedantBhosle31 <bvedant8@gmail.com>
2024-07-22 12:37:49 +0530

    Added Test for db setup

---
+++ tests/db_test.py
@@ -0,0 +1,45 @@
+from pkg.db.db import FileManager
+from pkg.db.progressEnum import ProgressEnum
+
+
+def db_test():
+    fileManager = FileManager()
+    # Create
+    new_record = fileManager.create_file_record(
+        path="/path/to/",
+        filename="file.txt",
+        sha256="d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2",
+        progress=ProgressEnum.NOTSTARTED,
+    )
+    print(f"Created: {new_record}")
+
+    # Read all
+    file_records = fileManager.read_all_file_records()
+    print("All file records:", file_records)
+
+    # Read by ID
+    record = fileManager.read_file_record_by_id(new_record.id)
+    print(f"Read by ID: {record}")
+
+    # Update
+    updated_record = fileManager.update_file_record(
+        record_id=new_record.id,
+        path="/new/path/to/",
+        filename="new_file.txt",
+        sha256="e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3",
+        progress=ProgressEnum.INPROGRESS,
+    )
+    print(f"Updated: {updated_record}")
+
+    # Delete
+    deleted_record = fileManager.delete_file_record(new_record.id)
+    print(f"Deleted: {deleted_record}")
+
+    # Verify deletion
+    file_records = fileManager.read_all_file_records()
+    print("All file records after deletion:", file_records)
+
+    test_record = fileManager.read_file_record_by_path_and_filename(
+        "/sites/NAF/Shared Documents/Test2", "test.txt"
+    )
+    print(f"Read by path and filename: {test_record.sha256}")
